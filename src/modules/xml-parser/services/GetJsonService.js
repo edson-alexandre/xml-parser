bef5b26bf53869632f9d84e69ef44cf9b26db999
Edson Alexandre <edson.a.s.silveira@gmail.com>
2023-07-16 13:09:52 -0300

    Convertendo json convertido para camelCase

--- src/modules/xml-parser/services/GetJsonService.js
+++ src/modules/xml-parser/services/GetJsonService.js
@@ -1,41 +1,26 @@
 const { XMLParser } = require("fast-xml-parser");
-
+const { camelCase } = require("lodash");
 const xml = require("./xml");
 module.exports = class GetJsonService {
   async execute() {
     const parser = new XMLParser();
     const obj = parser.parse(xml);
     delete obj["?xml"];
-    return this.toCamelCase(obj);
+    return this.camelizeKeys(obj);
   }
 
-  toCamelCase(obj) {
-    let newO, origKey, newKey, value;
-    if (obj instanceof Array) {
-      return obj.map(function (value) {
-        if (typeof value === "object") {
-          value = this.toCamelCase(value);
-        }
-        return value;
-      });
-    } else {
-      newO = {};
-      for (origKey in obj) {
-        if (obj.hasOwnProperty(origKey)) {
-          newKey = (
-            origKey.charAt(0).toLowerCase() + origKey.slice(1) || origKey
-          ).toString();
-          value = obj[origKey];
-          if (
-            value instanceof Array ||
-            (value !== null && value.constructor === Object)
-          ) {
-            value = this.toCamelCase(value);
-          }
-          newO[newKey] = value;
-        }
-      }
+  camelizeKeys = (obj) => {
+    if (Array.isArray(obj)) {
+      return obj.map((v) => this.camelizeKeys(v));
+    } else if (obj != null && obj.constructor === Object) {
+      return Object.keys(obj).reduce(
+        (result, key) => ({
+          ...result,
+          [camelCase(key)]: this.camelizeKeys(obj[key]),
+        }),
+        {}
+      );
     }
-    return newO;
-  }
+    return obj;
+  };
 };
