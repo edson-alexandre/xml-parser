e6c5e12be1f7e843e19e4121b413b019cff6c531
Edson Alexandre <edson.a.s.silveira@gmail.com>
2023-07-16 13:19:59 -0300

    Não exportando função auxiliar da clase GetJsonService

--- src/modules/xml-parser/services/GetJsonService.js
+++ src/modules/xml-parser/services/GetJsonService.js
@@ -1,26 +1,27 @@
 const { XMLParser } = require("fast-xml-parser");
 const { camelCase } = require("lodash");
 const xml = require("./xml");
+
 module.exports = class GetJsonService {
   async execute() {
     const parser = new XMLParser();
     const obj = parser.parse(xml);
     delete obj["?xml"];
-    return this.toCamelCase(obj);
+    return toCamelCase(obj);
   }
+};
 
-  toCamelCase = (value) => {
-    if (Array.isArray(value)) {
-      return value.map((v) => this.toCamelCase(v));
-    } else if (value != null && value.constructor === Object) {
-      return Object.keys(value).reduce(
-        (result, key) => ({
-          ...result,
-          [camelCase(key)]: this.toCamelCase(value[key]),
-        }),
-        {}
-      );
-    }
-    return value;
-  };
+toCamelCase = (value) => {
+  if (Array.isArray(value)) {
+    return value.map((v) => toCamelCase(v));
+  } else if (value != null && value.constructor === Object) {
+    return Object.keys(value).reduce(
+      (result, key) => ({
+        ...result,
+        [camelCase(key)]: toCamelCase(value[key]),
+      }),
+      {}
+    );
+  }
+  return value;
 };
